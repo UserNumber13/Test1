cdd244204b67568d2c9e7de7c9538f6b1e52abba
UserNumber13 <86889244+UserNumber13@users.noreply.github.com>
2021-07-04 12:54:31 +0300

    Add files via upload

---
+++ 1/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char a = '3';
+        char b = 'c';
+        char c = 'd';
+        System.out.println((a + b + c)\3);
+    }
+}
